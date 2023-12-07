bd1010dbf66170633eb2021293102419e22ceb0d
Sainath Reddy <37265003+sainath-reddiee@users.noreply.github.com>
2023-12-07 11:11:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.title('sai');
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
